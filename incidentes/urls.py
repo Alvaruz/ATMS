15e29798f4bbf247c79abd93f6431c0b913daf11
Alvaro Cárdenas <alvaruz101@gmail.com>
2018-11-30 14:06:50 -0300

    vistas estadisticas arreglo
    arregladas las vistas locales de estadisticas

--- incidentes/urls.py
+++ incidentes/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('tickets/estadisticas_dia/', views.estadisticas_dia, name='estadisticas_dia'),
     path('tickets/estadisticas_mes/', views.estadisticas_mes, name='estadisticas_mes'),
     path('tickets/estadisticas_total/', views.estadisticas_total, name='estadisticas_total'),
-    path('comunicaciones/estadisticas_dia/', views.estadisticas_dia, name='comunicaciones_estadisticas_dia'),
-    path('comunicaciones/estadisticas_mes/', views.estadisticas_mes, name='comunicaciones_estadisticas_mes'),
+    path('comunicaciones/estadisticas_dia/', views.comunicaciones_estadisticas_dia, name='comunicaciones_estadisticas_dia'),
+    path('comunicaciones/estadisticas_mes/', views.comunicaciones_estadisticas_mes, name='comunicaciones_estadisticas_mes'),
     # path('tickets/api/estadisticas',views.apimes, name="apimes"),
 ]
